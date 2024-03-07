4684e3247ee581467c71fff6e524e3c3d4d83d9f
AlbouabidiMouadh <albouabidimouadh@gmail.com>
2024-03-07 12:49:29 +0100

    image tools app

--- client/website/src/app/apps/image-tools/page.tsx
+++ client/website/src/app/apps/image-tools/page.tsx
@@ -1,14 +1,37 @@
-import ImageAppsHeader from '@/components/appsPages/ImageAppsHeader'
-import MainContainer from '@/containers/MainContainer'
-import React from 'react'
-
+import ImageApp from "@/components/appsPages/ImageApp";
+import ImageAppsHeader from "@/components/appsPages/ImageAppsHeader";
+import MainContainer from "@/containers/MainContainer";
+import React from "react";
+import picture from "@/public/illustrations/Char 03.png";
 const index = () => {
+  const apps = [
+    {
+      id: "0",
+      image: picture.src,
+      title: "app title",
+      url: "string",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus quam, fugiat inventore itaque ipsa provident soluta dolorem ab earum facere iusto quo ex fugit expedita sapiente? Quas, tenetur natus? Dolore.",
+    },
+  ];
+
   return (
     <MainContainer>
-      <ImageAppsHeader/>
-      this is the image apps page
+      <ImageAppsHeader />
+      {/* this is the image apps page */}
+      {apps.map((item) => {
+        return (
+          <ImageApp
+            id={item.id}
+            image={item.image}
+            title={item.title}
+            url={item.url}
+            description={item.description}
+          />
+        );
+      })}
     </MainContainer>
-  )
-}
+  );
+};
 
-export default index
+export default index;
--- client/website/src/components/appsPages/ImageAppsHeader.tsx
+++ client/website/src/components/appsPages/ImageAppsHeader.tsx
@@ -1,18 +1,23 @@
 import Image from "next/image";
 import React from "react";
-import picture from "@/public/illustrations/Char 08.png";
+import picture from "@/public/illustrations/Char 11.png";
 const ImageAppsHeader = () => {
   return (
     <div
       style={{
+        height: "300px",
         backgroundColor: "black",
-        height: "400px",
         color: "white",
+        padding: "50px 15%",
         display: "flex",
+        flexDirection: "row",
         justifyContent: "space-between",
+        flexWrap: "wrap",
+        alignContent: "center",
+        alignItems: "center",
       }}
     >
-      <Image src={picture} alt="image-tools-header-picture" />
+      <Image src={picture} alt="image-tools-header" height={250} />
       <div>Discover all image apps and tools</div>
     </div>
   );
